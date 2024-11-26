f56ae758f31914cee296daa41ff571f487e8556d
Anna40p <vladvornik18@gmail.com>
2024-11-26 17:51:17 +0300

    Api timeout.

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,10 +1,15 @@
 import os
+from time import sleep
 
 import pytest
-
 from api.client import DiscordClient
 
 
 @pytest.fixture(scope="session")
 def client():
     return DiscordClient(token=os.getenv("DISCORD_BOT_TOKEN"))
+
+
+@pytest.fixture(scope="function", autouse=True)
+def timeout():
+    sleep(2)
